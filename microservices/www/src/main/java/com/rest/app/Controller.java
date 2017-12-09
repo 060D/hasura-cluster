931c9daba5da5de4032a5822aa39e6a6d8537bfc
falguni <falguni@localhost.localdomain>
2017-12-09 20:58:44 +0530

    changes

--- microservices/www/src/main/java/com/rest/app/Controller.java
+++ microservices/www/src/main/java/com/rest/app/Controller.java
@@ -1,15 +1,29 @@
 package com.rest.app;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.rest.app.dao.DAO;
 
 @RestController
 @RequestMapping("/")
 public class Controller {
 	
-	@RequestMapping(path="/hello")
+	@RequestMapping(path="/")
 	public String hello() {
 		return "Welcome" ; 
 	}
-
+	
+	@RequestMapping(path="/stores", method= {RequestMethod.GET})
+	public String getStores() {
+		
+		DAO dao = new DAO();
+		
+		return dao.getStores(null); 
+	}
+	
+	
 }
---
+++ microservices/www/src/main/java/com/rest/app/dao/DAO.java
@@ -0,0 +1,42 @@
+package com.rest.app.dao;
+
+import java.util.HashMap;
+
+import com.rest.app.util.DataAPIUtil;
+
+public class DAO {
+	
+	public String getStores(String storeType) {
+		
+		HashMap<String, Object> params = new HashMap<String, Object>();
+		params.put("table", "stores");
+		params.put("columns", new String[] {"*"});
+		
+		//System.out.println(selectDataFromTable(params));
+		
+		
+		if(storeType!=null)
+		{
+			HashMap<String, String> whereCondition = new HashMap<String, String>();
+			whereCondition.put("type", storeType);
+			
+			params.put("where", whereCondition);
+			
+		}
+		
+		String data = DataAPIUtil.selectDataFromTable(params);
+		
+		return data;
+		
+	}
+	
+	//test
+	
+	public static void main(String[] args) {
+		
+		DAO dao = new DAO();
+		dao.getStores("Flower");
+		
+	}
+
+}
